b3e44f69843b2fe1d0466e8c9eaa4092b5208dcc
ZheKOs <e.evdokimov79@gmail.com>
2015-06-04 21:56:40 +0300

    Create base of first activity and layout

--- app/src/main/java/com/evdokimov/eugene/mobilecoach/MainActivity.java
+++ app/src/main/java/com/evdokimov/eugene/mobilecoach/MainActivity.java
@@ -1,9 +1,16 @@
 package com.evdokimov.eugene.mobilecoach;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.rey.material.app.Dialog;
+import com.rey.material.app.TimePickerDialog;
+import com.rey.material.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,29 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Context context = this;
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("ТРЕНИРОВКА"));
+        tabLayout.addTab(tabLayout.newTab().setText("ПИТАНИЕ"));
+        tabLayout.addTab(tabLayout.newTab().setText("СТАТИСТИКА"));
+
+
+        ImageButton btnStart = (ImageButton) findViewById(R.id.btn_set_time);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn_set_time:
+                        Dialog dialog = new TimePickerDialog(context);
+
+                        dialog.show();
+                        break;
+                }
+            }
+        });
+
     }
 
 }
