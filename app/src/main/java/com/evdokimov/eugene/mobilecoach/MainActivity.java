2bbb3c1dc5b950efe753bdce35a444d924a4eb7f
ZheKOs <e.evdokimov79@gmail.com>
2015-06-04 22:31:13 +0300

    View of training fragment is done.

--- app/src/main/java/com/evdokimov/eugene/mobilecoach/MainActivity.java
+++ app/src/main/java/com/evdokimov/eugene/mobilecoach/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.rey.material.app.Dialog;
 import com.rey.material.app.TimePickerDialog;
@@ -28,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("ПИТАНИЕ"));
         tabLayout.addTab(tabLayout.newTab().setText("СТАТИСТИКА"));
 
+        String[] workouts = {"УПРАЖНЕНИЕ","УПРАЖНЕНИЕ","УПРАЖНЕНИЕ","УПРАЖНЕНИЕ","УПРАЖНЕНИЕ","УПРАЖНЕНИЕ","УПРАЖНЕНИЕ"};
+
+        ListView lv_plan_workouts = (ListView) findViewById(R.id.lv_main_workouts);
+        WorkoutsAdapter workoutsAdapter = new WorkoutsAdapter(context, workouts);
+        lv_plan_workouts.setAdapter(workoutsAdapter);
+
 
         ImageButton btnStart = (ImageButton) findViewById(R.id.btn_set_time);
         btnStart.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +54,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    class WorkoutsAdapter extends ArrayAdapter<String>
+    {
+
+        private Context context;
+        private String[] strings;
+
+        public WorkoutsAdapter(Context context, String[] objects) {
+            super(context, R.layout.row_main_training, objects);
+
+            this.context = context;
+            this.strings = objects;
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.row_main_training,parent,false);
+
+            TextView tvWorkoutName = (TextView) rowView.findViewById(R.id.main_row_tv_workout);
+            TextView tvTimesToDo = (TextView) rowView.findViewById(R.id.main_row_times);
+
+            tvWorkoutName.setText(strings[position]);
+
+            return rowView;
+        }
     }
 
 }
